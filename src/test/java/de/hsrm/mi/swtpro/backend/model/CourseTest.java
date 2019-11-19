fbe99c3bd41bfd26c2addc0ce26b7b8dd17c7499
Lukas <dr.kaese@live.com>
2019-11-19 17:43:38 +0100

    + CourseComponentTest + CourseTest + CurriculumTest

--- src/test/java/de/hsrm/mi/swtpro/backend/model/CourseTest.java
+++ src/test/java/de/hsrm/mi/swtpro/backend/model/CourseTest.java
@@ -4,6 +4,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.hamcrest.Matchers.hasItem;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
@@ -14,6 +17,8 @@ public class CourseTest {
 
     private Course course;
     private User user;
+    private Module module;
+    private Set<Module> modules;
 
     @Before
     public void setUp(){
@@ -21,6 +26,9 @@ public class CourseTest {
         course = new Course.Builder("HCI")
                 .withOwner(user)
                 .build();
+        modules = new HashSet<>();
+        module = new Module.Builder("erleuchtendes Module").build();
+        modules.add(module);
     }
 
     @Test
@@ -29,15 +37,37 @@ public class CourseTest {
     }
 
     @Test
-    public void whenGetUser_thenReturnUser(){
+    public void whenGetOwner_thenReturnUser(){
         assertEquals("Gustav",course.getOwner().getFirstName());
     }
 
     @Test
     public void whenSetTerm_thenSaveTerm(){
         Term term = new Term.Builder().inPeriod(1).build();
+        Set<Term> terms = new HashSet<>();
+        terms.add(term);
+        course.setTerms(terms);
+
         assertThat(course.getTerms(),hasItem(term));
     }
 
+    @Test
+    public void whenSetTitle_thenSaveTitle(){
+        course.setTitle("RIP");
+        assertEquals("RIP",course.getTitle());
+    }
+
+    @Test
+    public void whenSetOwner_thenSaveOwner(){
+        User user = new User("Karl","Karlson","kool","wort",false);
+        course.setOwner(user);
+        assertEquals("Karlson",course.getOwner().getLastName());
+    }
+
+    @Test
+    public void whenSetModule_thenSaveModule(){
+        course.setModules(modules);
+        assertThat(course.getModules(),hasItem(module));
+    }
 
 }
--- src/test/java/de/hsrm/mi/swtpro/backend/model/CurriculumTest.java
+++ src/test/java/de/hsrm/mi/swtpro/backend/model/CurriculumTest.java
@@ -2,13 +2,59 @@ package de.hsrm.mi.swtpro.backend.model;
 
 
 import org.junit.Before;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 @SpringBootTest
 public class CurriculumTest {
 
+    private Curriculum curriculum;
+    private ExamRegulation examRegulation;
+    private Set<Module> moduleList;
+    private Module module;
+
     @Before
     public void setUp(){
+        examRegulation = new ExamRegulation.Builder(Date.valueOf(LocalDate.MAX)).build();
+        module = new Module.Builder("Mathe 3").build();
+        curriculum = new Curriculum.Builder(examRegulation)
+                .hasModule(module)
+                .build();
 
+        moduleList = new HashSet<>();
+    }
+
+    @Test
+    public void whenGetExamRegulation_thenReturnExamRegulation(){
+        assertEquals(Date.valueOf(LocalDate.MAX),curriculum.getExamRegulation().getDate());
+    }
+
+    @Test
+    public void whenGetModules_thenReturnModules(){
+        assertThat(curriculum.getModules(),hasItem(module));
+    }
+
+    @Test
+    public void whenSetId_thenSaveId(){
+        curriculum.setId(17);
+        assertEquals(17,curriculum.getId());
+    }
+
+    @Test
+    public void whenSetModules_thenSaveModules(){
+        module = new Module.Builder("Mathe 4").build();
+
+        moduleList.add(module);
+        curriculum.setModules(moduleList);
+        assertThat(curriculum.getModules(),hasItem(module));
     }
 }
