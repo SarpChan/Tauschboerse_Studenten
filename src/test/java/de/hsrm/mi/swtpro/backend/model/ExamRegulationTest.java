24229839ad8ecf2a19dcd2476d9d9a3ea176f544
Lukas <dr.kaese@live.com>
2019-11-19 18:32:53 +0100

    + ExamRegulationTest +FieldOfStudyTest

--- src/test/java/de/hsrm/mi/swtpro/backend/model/ExamRegulationTest.java
+++ src/test/java/de/hsrm/mi/swtpro/backend/model/ExamRegulationTest.java
@@ -2,13 +2,59 @@ package de.hsrm.mi.swtpro.backend.model;
 
 
 import org.junit.Before;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
 public class ExamRegulationTest {
 
+    private ExamRegulation examRegulation;
+    private StudyProgram studyProgram;
+    private Curriculum curriculum;
+
     @Before
     public void setUp(){
-
+        studyProgram = new StudyProgram.Builder("Medieninformatik","Bachlor").build();
+        examRegulation = new ExamRegulation.Builder(Date.valueOf(LocalDate.of(2017,7,17)))
+                .forStudyProgram(studyProgram)
+                .build();
+        curriculum = new Curriculum.Builder(examRegulation).build();
     }
+
+    @Test
+    public void whenGetDate_thenReturnDate(){
+        assertEquals(Date.valueOf(LocalDate.of(2017,7,17)).toString(),examRegulation.getDate().toString());
+    }
+
+    @Test
+    public void whenGetStudyProgram_thenReturnStudyProgram(){
+        assertEquals("Medieninformatik",examRegulation.getStudyProgram().getTitle());
+    }
+
+    @Test
+    public void whenSetId_thenSaveId(){
+        examRegulation.setId(17);
+        assertEquals(17,examRegulation.getId());
+    }
+
+    @Test
+    public void whenSetDate_thenSaveDate(){
+        examRegulation.setDate(Date.valueOf(LocalDate.of(2020,7,21)));
+        assertEquals(Date.valueOf(LocalDate.of(2020,7,21)).toString(),examRegulation.getDate().toString());
+    }
+
+    @Test
+    public void whenSetStudyProgram_thenSaveStudyProgram(){
+        studyProgram = new StudyProgram.Builder("anderer Studiengang","Master").build();
+
+        examRegulation.setStudyProgram(studyProgram);
+        assertEquals("anderer Studiengang",examRegulation.getStudyProgram().getTitle());
+    }
+
+
 }
--- src/test/java/de/hsrm/mi/swtpro/backend/model/FieldOfStudyTest.java
+++ src/test/java/de/hsrm/mi/swtpro/backend/model/FieldOfStudyTest.java
@@ -2,13 +2,61 @@ package de.hsrm.mi.swtpro.backend.model;
 
 
 import org.junit.Before;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.junit.Assert.*;
 
 @SpringBootTest
 public class FieldOfStudyTest {
 
+    FieldOfStudy fieldOfStudy;
+    StudyProgram studyProgram;
+    University university;
+    Term term;
+
     @Before
     public void setUp(){
-
+        university = new University("Störtebecker Hochschule","Adelheidstraße 84, 65185 Wiesbaden");
+        studyProgram = new StudyProgram.Builder("MI","Master").build();
+        term = new Term.Builder().withEndDate(Date.valueOf(LocalDate.MIN)).build();
+        fieldOfStudy = new FieldOfStudy("Informatik");
     }
+
+    @Test
+    public void whenGetTitle_thenReturnTitle(){
+        assertEquals("MI",fieldOfStudy.getTitle());
+    }
+
+    @Test
+    public void whenSetTitle_thenSaveTitle(){
+        fieldOfStudy.setTitle("Buchwissenschaften");
+        assertEquals("Buchwissenschaften",fieldOfStudy.getTitle());
+    }
+
+    @Test
+    public void whenSetStudyProgram_thenSaveStudyProgram(){
+        List<StudyProgram> studyProgramList = new ArrayList<>();
+        studyProgramList.add(studyProgram);
+        fieldOfStudy.setStudyProgram(studyProgramList);
+        assertThat(fieldOfStudy.getStudyProgram(),hasItem(studyProgram));
+    }
+
+    @Test
+    public void whenSetUniversity_thenSaveUniversity(){
+        fieldOfStudy.setUniversity(university);
+        assertTrue("kein Getter fuer die University",false);
+    }
+
+    @Test
+    public void whenSetTerm_thenSaveTerm(){
+        assertTrue("Term -> Attribut fehlt ",false);
+    }
+
 }
