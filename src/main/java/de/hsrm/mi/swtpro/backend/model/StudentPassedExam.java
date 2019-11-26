20182767064a9b3bae6f28329dd1985a153618dc
Lukas <dr.kaese@live.com>
2019-11-27 00:27:17 +0100

    ! jeweilige models sind auf public gesetzt worden (warte auf aender in feature Branche) + Student Spezial Relation Repository

--- src/main/java/de/hsrm/mi/swtpro/backend/model/StudentPassedExam.java
+++ src/main/java/de/hsrm/mi/swtpro/backend/model/StudentPassedExam.java
@@ -1,19 +1,19 @@
 package de.hsrm.mi.swtpro.backend.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
 @Entity
 @AllArgsConstructor
 @Builder
-class StudentPassedExam {
+public class StudentPassedExam {
 
     @Id
     @Getter @Setter
