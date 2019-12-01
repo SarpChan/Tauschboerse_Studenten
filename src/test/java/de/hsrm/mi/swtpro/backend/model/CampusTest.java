0a1259f24f9f2dcac1ded39556eba5ec25fcc66c
Lukas <dr.kaese@live.com>
2019-12-01 18:39:08 +0100

    Campus complete

--- src/test/java/de/hsrm/mi/swtpro/backend/model/CampusTest.java
+++ src/test/java/de/hsrm/mi/swtpro/backend/model/CampusTest.java
@@ -23,11 +23,22 @@ public class CampusTest {
                 .address("Champ de Mars, 5 Avenue Anatole France, 75007 Paris, Frankreich")
                 .build();
 
+        Building building = Building.builder()
+                .id(42L)
+                .build();
+
         campus = Campus.builder()
+                .id(17L)
                 .name("Unter den Eichen")
+                .building(building)
                 .address("Platz der Republik 1, 11011 Berlin")
                 .university(university)
                 .build();
+    }
+
+    @Test
+    public void whenGetId_thenReturnId(){
+        assertEquals(campus.getId(),17L);
     }
 
     @Test
@@ -43,6 +54,39 @@ public class CampusTest {
     @Test
     public void whenGetUniversity_thenReturnUniversity(){
         assertEquals(campus.getUniversity().getName(),"FH Kiel");
+    }
+
+    @Test
+    public void whenGetBuilding_thenReturnBuilding(){
+        assertThat(campus.getBuildings(),hasItems(
+                hasProperty("id",is(42L))));
+    }
+
+    @Test
+    public void whenSetId_thenSaveId(){
+        assertEquals(campus.getId(),17L);
+    }
+
+    @Test
+    public void whenSetName_thenSaveName(){
+        campus.setName("Unter den Eichen");
+        assertEquals("Unter den Eichen",campus.getName());
+    }
+
+    @Test
+    public void whenSetAddress_thenSaveAddress(){
+        campus.setAddress("Bahnhofstraße 50, 65185 Wiesbaden");
+        assertEquals("Bahnhofstraße 50, 65185 Wiesbaden",campus.getAddress());
+    }
+
+    @Test
+    public void whenSetUniversity_thenSaveUniversity(){
+        University university = University.builder()
+                .id(65L)
+                .name("Hauptschule RheinMain")
+                .build();
+        campus.setUniversity(university);
+        assertThat(campus.getUniversity(),hasProperty("id",is(65L)));
     }
 
     @Test
