0140bedc2b21724d126bd44dec1e5c6a105d4063
jmuel004 <julius.mueller@student.hs-rm.de>
2019-12-02 14:35:06 +0100

    JSON Generator now persists his Entitys

--- src/main/java/de/hsrm/mi/swtpro/backend/service/JSONGenerator.java
+++ src/main/java/de/hsrm/mi/swtpro/backend/service/JSONGenerator.java
@@ -3,60 +3,99 @@ package de.hsrm.mi.swtpro.backend.service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.hsrm.mi.swtpro.backend.model.*;
 import de.hsrm.mi.swtpro.backend.model.Module;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import javax.persistence.*;
 import java.io.File;
 import java.io.IOException;
 import java.sql.Date;
 import java.time.DayOfWeek;
 import java.time.LocalTime;
 
-import lombok.Builder;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
+//@Transactional
 public class JSONGenerator {
+
+    @Autowired
+    EntityManagerFactory emf;
+    private EntityManager entityManager;
+
+    /*public  JSONGenerator(){
+
+    }*/
 
     @PostConstruct
     //@EventListener(ApplicationReadyEvent.class)
     private void init() {
+        entityManager = emf.createEntityManager();
 
         University uni = University.builder().name("Hochschule RheinMain").address("Kurt-Schuhmacher-Ring 18").build();
+        entityManager.persist(uni);
         Campus ude = Campus.builder().name("Unter den Eichen").address("Unter den Eichen 6").university(uni).build();
+        entityManager.persist(ude);
         Building dBuilding = Building.builder().name("Gebäude D").campus(ude).build();
+        entityManager.persist(dBuilding);
+
+
         Room r14 = Room.builder().number(14).seats(60).building(dBuilding).build();
+        entityManager.persist(r14);
 
 
         Term ws1920 = Term.builder().startDate(Date.valueOf("2019-10-01")).endDate(Date.valueOf("2020-03-31")).period(1).build();
+        entityManager.persist(ws1920);
         ExamRegulation po2017 = ExamRegulation.builder().date(Date.valueOf("2017-10-01")).build();
+        entityManager.persist(po2017);
 
         User testUser = User.builder().firstName("Test").lastName("User").loginName("testUser").password("test").build();
+        entityManager.persist(testUser);
         Student testStudent = Student.builder().user(testUser).enrolementNumber(123456)
                 .enrolmentTerm(ws1920).mail("test@test.com").examRegulation(po2017).build();
+        entityManager.persist(testStudent);
         Lecturer testLecturer = Lecturer.builder().priviledge(1).user(testUser).build();
+        entityManager.persist(testLecturer);
 
 
 
         StudyProgram medieninformatik = StudyProgram.builder().title("Medieninformatik").degree("Bachlor").build();
+        entityManager.persist(medieninformatik);
         FieldOfStudy dcsm = FieldOfStudy.builder().title("DCSM").build();
+        entityManager.persist(dcsm);
         Curriculum curriculum = Curriculum.builder().examRegulation(po2017).termPeriod(1).build();
+        entityManager.persist(curriculum);
         Module mProgrammieren3 = Module.builder().title("Programmieren 3").creditPoints(5).period(1).build();
+        entityManager.persist(mProgrammieren3);
         ModuleInCurriculum moduleInCurriculum = ModuleInCurriculum.builder().curriculum(curriculum).module(mProgrammieren3).termPeriod(1).build();
+        entityManager.persist(moduleInCurriculum);
         Course cProgrammieren3 = Course.builder().title("Programmieren 3").owner(testUser).build();
+        entityManager.persist(cProgrammieren3);
         CourseComponent prog3V = CourseComponent.builder().course(cProgrammieren3).creditPoints(5).type(CourseType.LECTURE).exam("Klausur").build();
+        entityManager.persist(prog3V);
         CourseComponent prog3P = CourseComponent.builder().course(cProgrammieren3).exam("Praktische Tätigkeit").type(CourseType.PRACTICE).creditPoints(0).build();
+        entityManager.persist(prog3P);
         Group prog3Vgroup = Group.builder().lecturer(testLecturer).slots(100).room(r14).term(ws1920).dayOfWeek((DayOfWeek.MONDAY)).groupChar('A')
                 .courseComponent(prog3V).startTime(LocalTime.of(10,00)).endTime(LocalTime.of(11,30)).build();
+        entityManager.persist(prog3Vgroup);
         Group prog3PgroupA = Group.builder().lecturer(testLecturer).slots(20).room(r14).term(ws1920).dayOfWeek((DayOfWeek.TUESDAY)).groupChar('A')
                 .courseComponent(prog3P).startTime(LocalTime.of(10,00)).endTime(LocalTime.of(11,30)).build();
+        entityManager.persist(prog3PgroupA);
         Group prog3PgroupB = Group.builder().lecturer(testLecturer).slots(20).room(r14).term(ws1920).dayOfWeek((DayOfWeek.WEDNESDAY)).groupChar('B')
                 .courseComponent(prog3P).startTime(LocalTime.of(10,00)).endTime(LocalTime.of(11,30)).build();
+        entityManager.persist(prog3PgroupB);
         Group prog3PgroupC = Group.builder().lecturer(testLecturer).slots(20).room(r14).term(ws1920).dayOfWeek((DayOfWeek.THURSDAY)).groupChar('C')
                 .courseComponent(prog3P).startTime(LocalTime.of(10,00)).endTime(LocalTime.of(11,30)).build();
+        entityManager.persist(prog3PgroupC);
         Group prog3PgroupD = Group.builder().lecturer(testLecturer).slots(20).room(r14).term(ws1920).dayOfWeek((DayOfWeek.FRIDAY)).groupChar('D')
                 .courseComponent(prog3P).startTime(LocalTime.of(10,00)).endTime(LocalTime.of(11,30)).build();
-       StudentAttendsCourse studentAttendsCourse = StudentAttendsCourse.builder().student(testStudent).course(cProgrammieren3).term(ws1920).build();
-
+        entityManager.persist(prog3PgroupD);
+        StudentAttendsCourse studentAttendsCourse = StudentAttendsCourse.builder().student(testStudent).course(cProgrammieren3).term(ws1920).build();
+        entityManager.persist(studentAttendsCourse);
 
         uni.getCampuses().add(ude);
         uni.getFieldsOfStudy().add(dcsm);
@@ -99,6 +138,11 @@ public class JSONGenerator {
 
         ws1920.addCourse(cProgrammieren3);
         po2017.setStudyProgram(medieninformatik);
+
+        //entityManager.persist(uni);
+
+        //entityManager.close();
+        //entityManager.flush();
 
         ObjectMapper objectMapper = new ObjectMapper();
         try {
