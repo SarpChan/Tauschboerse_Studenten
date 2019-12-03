72035231f2f6381f4f7500d5b9b3f7dbb2d505a1
oliverdaniel.folwarzny@student.hs-rm.de <ofolwarzny@coconut-capital.com>
2019-12-03 22:47:19 +0100

    Filter factory and logic

--- src/main/java/de/hsrm/mi/swtpro/backend/model/filter/Comparator.java
+++ src/main/java/de/hsrm/mi/swtpro/backend/model/filter/Comparator.java
@@ -1,6 +1,16 @@
 package de.hsrm.mi.swtpro.backend.model.filter;
 
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Comparator {
+    @Getter
+    @Setter
     ComparatorType comparatorType;
+    @Getter
+    @Setter
     Object comparatorValue;
 }
