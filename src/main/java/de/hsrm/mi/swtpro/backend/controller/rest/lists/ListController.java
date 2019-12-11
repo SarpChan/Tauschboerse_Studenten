b328e925949683bc904256d62a40ef4fba3ab86e
Denise Jäger <denise.jaeger@student.hs-rm.de>
2019-12-11 14:28:05 +0100

    list controller for timetable

--- src/main/java/de/hsrm/mi/swtpro/backend/controller/rest/lists/ListController.java
+++ src/main/java/de/hsrm/mi/swtpro/backend/controller/rest/lists/ListController.java
@@ -11,6 +11,7 @@ import de.hsrm.mi.swtpro.backend.service.repository.ModuleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,42 +27,22 @@ public class ListController {
     @Autowired
     ModuleRepository moduleRepository;
 
-    /*@GetMapping(path = "/course", consumes = "application/json", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Course> getCourse(@RequestBody Filter[] filters) {
-        List<Course> allCourses = courseRepository.findAll();
-        CourseFilterFactory filterFactory = CourseFilterFactory.builder().filters(filters).build();
-        allCourses = filterFactory.filterCourses(allCourses);
-        return allCourses;
-    }*/
-
-    /*@GetMapping(path = "/timetable", consumes = "application/json", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(path = "/timetable", consumes = "application/json", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Module> getModules(@RequestBody Filter[] filters) {
         List<Module> allModules = moduleRepository.findAll();
         ModuleFilterFactory filterFactory = ModuleFilterFactory.builder().filters(filters).build();
         allModules = filterFactory.filterModules(allModules);
-        return allModules;
-    }*/
-    @GetMapping(path = "/timetable", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Module> getModules() {
-        List<Module> allModules = moduleRepository.findAll();
-        //ModuleFilterFactory filterFactory = ModuleFilterFactory.builder().filters(filters).build();
-        //allModules = filterFactory.filterModules(allModules);
         for (Module module: allModules) {
             module.setModulesInCurriculum(null);
             for (Course course:module.getCourses()) {
                 course.setModules(null);
                 course.setTerms(null);
                 course.setStudentAttendsCourses(null);
-                //TODO
                 course.setOwner(null);
                 for(CourseComponent courseComponent:course.getCourseComponents()){
                     courseComponent.setCourse(null);
                     for(Group group:courseComponent.getGroups()){
                         group.setStudents(null);
-                        //TODO
-                        //group.setLecturer(null);
-                        //TODO
-                        //group.setRoom(null);
                         group.setPrioritizeGroups(null);
                         group.setTerm(null);
                         group.setCourseComponent(null);
