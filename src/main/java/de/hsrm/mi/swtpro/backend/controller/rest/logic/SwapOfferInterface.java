4f7f7da7444bfc1a158dbe55f7069b04765fd383
Tobias Thiel <tobias.thiel@student.hs-rm.de>
2019-12-16 13:44:21 +0100

    WIP SwapOfferInterface
    Signed-off-by: Tobias Thiel <tobias.thiel@student.hs-rm.de>

--- src/main/java/de/hsrm/mi/swtpro/backend/controller/rest/logic/SwapOfferInterface.java
+++ src/main/java/de/hsrm/mi/swtpro/backend/controller/rest/logic/SwapOfferInterface.java
@@ -2,19 +2,10 @@ package de.hsrm.mi.swtpro.backend.controller.rest.logic;
 
 import de.hsrm.mi.swtpro.backend.controller.rest.crud.GroupCrudController;
 import de.hsrm.mi.swtpro.backend.controller.rest.crud.StudentCrudController;
-import de.hsrm.mi.swtpro.backend.model.Student;
-import de.hsrm.mi.swtpro.backend.model.SwapOffer;
-import de.hsrm.mi.swtpro.backend.model.requestModel.SwapOfferRequest;
 import de.hsrm.mi.swtpro.backend.service.repository.SwapOfferRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.sql.Timestamp;
-import java.time.Instant;
 
 @RestController
 @RequestMapping("/rest")
@@ -25,7 +16,9 @@ public class SwapOfferInterface {
     StudentCrudController studentCrudController;
     @Autowired
     GroupCrudController groupCrudController;
-
+/*
+    @Autowired
+    SwapOfferService swapOfferService;*/
     /**
      * Iterates over list of groups to change, checks if recent iterations already created a match if not then create a SwapOffer for each Element
      * e.g. A->B , A->C , A->D ,....
@@ -33,10 +26,11 @@ public class SwapOfferInterface {
      * @param swapOfferRequest
      * @return boolean
      */
+        /*
     @PostMapping(path = "/swapoffer/insert", consumes = "application/json", produces = MediaType.APPLICATION_JSON_VALUE)
     public boolean checkAndInsertSwapOffer(@RequestBody SwapOfferRequest swapOfferRequest) {
-        for (long swapOfferToGroupID : swapOfferRequest.getToGroupsID()) {
-            if (swapOfferService.isMatched(swapOfferToGroupID)) {
+    for (long swapOfferToGroupID : swapOfferRequest.getToGroupsID()) {
+            if (swapOfferService.isMatched(swapOfferRequest.getOwnerEnrollmentNumber(),swapOfferRequest.getFromGroupsID(),swapOfferToGroupID)) {
                 return true;
             } else {
                 Timestamp date = Timestamp.from(Instant.now());
@@ -50,5 +44,6 @@ public class SwapOfferInterface {
             }
         }
         return true;
-    }
+    }*/
+
 }
