90b489993dccdb4350071f668ad898a0c46009e8
Denise Jäger <denise.jaeger@student.hs-rm.de>
2019-12-22 16:23:00 +0100

    added timetable model

---
+++ src/main/java/de/hsrm/mi/swtpro/backend/model/Timetable.java
@@ -0,0 +1,34 @@
+package de.hsrm.mi.swtpro.backend.model;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+import lombok.*;
+
+@AllArgsConstructor
+@Builder
+public class Timetable {
+    @Getter @Setter
+    private long courseComponentID;
+    @Getter @Setter
+    private long groupID;
+    @Getter @Setter
+    private LocalTime startTime;
+    @Getter @Setter
+    private LocalTime endTime;
+    @Getter @Setter
+    private DayOfWeek dayOfWeek;
+    @Getter @Setter
+    private String lecturerName;
+    @Getter @Setter
+    private String lecturerNameAbbreviation;
+    @Getter @Setter
+    private String moduleTitle;
+    @Getter @Setter
+    private String moduleTitleAbbreviation;
+    @Getter @Setter
+    private char groupChar;
+    @Getter @Setter
+    private CourseType courseType;
+}
+
