941dab4cc04b8a9d2922208c9791cd2da0e4c10e
Tobi <tobias.thiel@student.hs-rm.de>
2019-12-31 17:21:41 +0100

    WIP SwapOfferService and Interface

--- src/main/java/de/hsrm/mi/swtpro/backend/controller/rest/logic/SwapOfferInterface.java
+++ src/main/java/de/hsrm/mi/swtpro/backend/controller/rest/logic/SwapOfferInterface.java
@@ -1,24 +1,44 @@
+
 package de.hsrm.mi.swtpro.backend.controller.rest.logic;
 
 import de.hsrm.mi.swtpro.backend.controller.rest.crud.GroupCrudController;
 import de.hsrm.mi.swtpro.backend.controller.rest.crud.StudentCrudController;
+import de.hsrm.mi.swtpro.backend.model.SwapOffer;
+import de.hsrm.mi.swtpro.backend.model.requestModel.SwapOfferRequest;
+import de.hsrm.mi.swtpro.backend.service.SwapOfferService;
+import de.hsrm.mi.swtpro.backend.service.repository.GroupRepository;
+import de.hsrm.mi.swtpro.backend.service.repository.StudentRepository;
 import de.hsrm.mi.swtpro.backend.service.repository.SwapOfferRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 @RestController
 @RequestMapping("/rest")
 public class SwapOfferInterface {
+
+    @Autowired
+    StudentRepository studentRepository;
+    @Autowired
+    GroupRepository groupRepository;
     @Autowired
     SwapOfferRepository swapOfferRepository;
     @Autowired
     StudentCrudController studentCrudController;
     @Autowired
     GroupCrudController groupCrudController;
-/*
+
     @Autowired
-    SwapOfferService swapOfferService;*/
+    SwapOfferService swapOfferService;
     /**
      * Iterates over list of groups to change, checks if recent iterations already created a match if not then create a SwapOffer for each Element
      * e.g. A->B , A->C , A->D ,....
@@ -26,15 +46,13 @@ public class SwapOfferInterface {
      * @param swapOfferRequest
      * @return boolean
      */
-        /*
+
     @PostMapping(path = "/swapoffer/insert", consumes = "application/json", produces = MediaType.APPLICATION_JSON_VALUE)
     public boolean checkAndInsertSwapOffer(@RequestBody SwapOfferRequest swapOfferRequest) {
-    for (long swapOfferToGroupID : swapOfferRequest.getToGroupsID()) {
+        for (long swapOfferToGroupID : swapOfferRequest.getToGroupsID()) {
             if (swapOfferService.isMatched(swapOfferRequest.getOwnerEnrollmentNumber(),swapOfferRequest.getFromGroupsID(),swapOfferToGroupID)) {
                 return true;
             } else {
-                Timestamp date = Timestamp.from(Instant.now());
-                Student student = studentCrudController.findStudent(swapOfferRequest.getOwnerEnrollmentNumber());
                 swapOfferRepository.save(new SwapOffer().builder()
                         .date(Timestamp.from(Instant.now()))
                         .student(studentCrudController.findStudent(swapOfferRequest.getOwnerEnrollmentNumber()))
@@ -44,6 +62,22 @@ public class SwapOfferInterface {
             }
         }
         return true;
-    }*/
+    }
+
+
+    @PostMapping(path = "/swapoffer/debug", consumes = "application/json", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<SwapOffer> debug(@RequestBody SwapOfferRequest swapOfferRequest) {
+        List<SwapOffer> swapofferList = swapOfferRepository.findByFromGroup(groupRepository.findById(swapOfferRequest.getToGroupsID()[0]).get());
+        Collections.sort(swapofferList, new Comparator<SwapOffer>() {
+            @Override
+            public int compare(SwapOffer a, SwapOffer b) {
+                return a.getDate().compareTo(b.getDate());
+            }
+        });
+    /*    swapOfferService.isMatched(swapOfferRequest.getOwnerEnrollmentNumber(),swapOfferRequest.getFromGroupsID(),swapOfferToGroupID)
+        Student A = studentRepository.findByEnrollmentNumber(swapOfferRequest.getOwnerEnrollmentNumber()).get();
+        Student B =  einzelnesSwapOffer.getStudent();*/
+        return swapofferList;
+    }
 
 }
--- src/main/java/de/hsrm/mi/swtpro/backend/service/SwapOfferService.java
+++ src/main/java/de/hsrm/mi/swtpro/backend/service/SwapOfferService.java
@@ -1,26 +1,57 @@
 package de.hsrm.mi.swtpro.backend.service;
 
 
+import de.hsrm.mi.swtpro.backend.model.Student;
 import de.hsrm.mi.swtpro.backend.model.SwapOffer;
+import de.hsrm.mi.swtpro.backend.service.repository.GroupRepository;
+import de.hsrm.mi.swtpro.backend.service.repository.StudentRepository;
+import de.hsrm.mi.swtpro.backend.service.repository.SwapOfferRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 /* TODO: Methode isMatched imnplementieren, bekommmt als Paramteter den SwappOfferRequest
 
 
  */
-@Autowired
-SwapOfferRepository swapOfferRepository;
-
+@Service
 public class SwapOfferService {
+    @Autowired
+    SwapOfferRepository swapOfferRepository;
+    @Autowired
+    StudentRepository studentRepository;
+    @Autowired
+    GroupRepository groupRepository;
 
     public boolean isMatched(int matrikelnummer, long startGruppe, long zielGruppe) {
-        List<SwapOffer> swapofferList = swapOfferRepository.findByFromGroup(zielGruppe);
-        Filter filter; //TODO: filter erstellen -> mit yen und olli reden
+        List<SwapOffer> swapofferList = swapOfferRepository.findByFromGroup(groupRepository.findById(zielGruppe).get());
+        Collections.sort(swapofferList, new Comparator<SwapOffer>() {
+            @Override
+            public int compare(SwapOffer a, SwapOffer b) {
+                return a.getDate().compareTo(b.getDate());
+            }
+        });
+      /*  Filter filter = Filter.builder().attribute("A").comparator(new Comparator<S>().setComparatorValue(1)).build(); //TODO: filter erstellen -> mit yen und olli reden
         filterFactory.filterForFromGroup(swapofferList, filter); //TODO: Bereits nach zielgruppe gefilterte List swapofferlist nach -> Liste an SwapOffers von Studenten die in der Zielgruppe sind Filtern ob diese in die Startgruppe möchten
+        */
         for(SwapOffer einzelnesSwapOffer: swapofferList){
-            if(einzelnesSwapOffer.getToGroup().getId() == startGruppe){ // OB ID der Zielgruppe mit der Startgruppe matcht
 
+            if(einzelnesSwapOffer.getToGroup().getId() == startGruppe){ // OB ID der Zielgruppe mit der Startgruppe matcht
+                Student A = studentRepository.findByEnrollmentNumber(matrikelnummer).get();
+                Student B = einzelnesSwapOffer.getStudent();
+                A.getGroups().forEach(group -> {
+                    if (group.getId() == startGruppe) group = groupRepository.findById(zielGruppe).get();
+                }); //TODO: Setzt des anfragenden Studis auf seine Zielgruppe
+                B.getGroups().forEach(group -> {
+                    if (group.getId() == startGruppe) group = groupRepository.findById(startGruppe).get();
+                });
                 //TODO: WENN Match -> beiden Studenten ihre Gruppe in dem Modul auf die neue Gruppe setzen und den Studenten in der Datenbank updaten. (Siehe StudentCrudController) und  return true
+                studentRepository.save(A);
+                studentRepository.save(B);
+
             }
 
         }
@@ -30,3 +61,4 @@ public class SwapOfferService {
     }
 
 }
+
