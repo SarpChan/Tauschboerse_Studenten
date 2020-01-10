fec95796b4c49becd1ac09a3c40c9000a36429c3
Leonie Hilgert <leonieluise.hilgert@student.hs-rm.de>
2020-01-10 21:32:08 +0100

    UserDetailsService created

---
+++ src/main/java/de/hsrm/mi/swtpro/backend/controller/login/config/JwtUserDetailsService.java
@@ -0,0 +1,43 @@
+package de.hsrm.mi.swtpro.backend.controller.login.config;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import de.hsrm.mi.swtpro.backend.model.User;
+import de.hsrm.mi.swtpro.backend.model.User.UserBuilder;
+import de.hsrm.mi.swtpro.backend.service.repository.UserRepository;
+
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> optionalUser = userRepository.findByLoginName(username);
+        if (!optionalUser.isPresent()) {
+            throw new UsernameNotFoundException(username);
+        }
+        User user = optionalUser.get();
+
+
+        //String u = ((user.getRole()).toString());
+        org.springframework.security.core.userdetails.User.UserBuilder builder = null;
+        if (user != null) {
+          builder = org.springframework.security.core.userdetails.User.withUsername(username);
+          builder.password(encoder.encode(user.getPassword()));
+          builder.roles("USER");
+        }
+        return builder.build();
+    }
+}
