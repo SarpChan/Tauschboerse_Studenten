6d463121f86a365a50a3f6e5fea885aff5f8e8e6
thilo <thilo@DESKTOP-RLU20C9>
2020-01-13 09:38:38 +0100

    Methode allSO

--- src/main/java/de/hsrm/mi/swtpro/backend/controller/rest/logic/SwapOfferInterface.java
+++ src/main/java/de/hsrm/mi/swtpro/backend/controller/rest/logic/SwapOfferInterface.java
@@ -14,10 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.Timestamp;
 import java.time.Instant;
@@ -108,5 +105,12 @@ public class SwapOfferInterface {
         Student B =  einzelnesSwapOffer.getStudent();*/
 
     }
+
+
+    @GetMapping(path = "/swapoffer/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<SwapOffer> selectAllSwapOffers(){
+        //:TODO Hier sollen alle Tauschangebote an das Frontend gesendent werden
+    }
+
 
 }
