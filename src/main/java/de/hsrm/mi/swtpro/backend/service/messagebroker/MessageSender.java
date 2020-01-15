563ba556e1eb5db668ea6c62a9219e7f5f9cd5d0
Julia Wirth <julia.wirth@student.hs-rm.de>
2020-01-15 13:50:09 +0100

    Merge TimetableUpdate

--- src/main/java/de/hsrm/mi/swtpro/backend/service/messagebroker/MessageSender.java
+++ src/main/java/de/hsrm/mi/swtpro/backend/service/messagebroker/MessageSender.java
@@ -1,9 +1,16 @@
 package de.hsrm.mi.swtpro.backend.service.messagebroker;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.activemq.command.ActiveMQTopic;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
@@ -19,18 +26,25 @@ public class MessageSender  {
 
     private final Logger logger = LoggerFactory.getLogger(MessageSender.class);
 
-    private final String QUEUENAME = "SwapOfferMessageQueue" + "userid";
-    private final ActiveMQQueue queue = new ActiveMQQueue(QUEUENAME);
+    private final Map<String, ActiveMQQueue> queueMap = new HashMap<String, ActiveMQQueue>();
 
     private final String TOPICNAME = "NewsMessageTopic";
-    private final ActiveMQQueue topic = new ActiveMQQueue(TOPICNAME);
+    private final ActiveMQTopic topic = new ActiveMQTopic(TOPICNAME);
 
     @Autowired
     private JmsTemplate jmsTemplate;
 
-    public void sendSwapOfferMessage(SwapOffer swapOffer) {
+    public void addSwapMessageQueue (List<String> userids) {
+        for(String userid : userids) {
+            String queuename = "SwapMessageQueue" + userid;
+            ActiveMQQueue queue = new ActiveMQQueue(queuename);
+            queueMap.put(queuename, queue);
+        }
+    }
+
+    public void sendSwapOfferMessage(SwapOffer swapOffer, String userid) {
         logger.info("sendingMessage: " + swapOffer);
-        jmsTemplate.send(queue, session -> session.createTextMessage("Du hast erfolgreich eine Gruppe getauscht." + swapOffer.toString()) );
+        jmsTemplate.send(queueMap.get("SwapMessageQueue" + userid), session -> session.createTextMessage("Du hast erfolgreich eine Gruppe getauscht." + swapOffer.toString()) );
     }
 
     public void sendNewsMessage() {
