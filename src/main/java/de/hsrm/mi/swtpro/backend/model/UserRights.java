eb7918189e49afd18e873e51a42eae533738cb39
Leonie Hilgert <leonieluise.hilgert@student.hs-rm.de>
2020-01-15 11:12:48 +0100

    small name changes UserRights

--- src/main/java/de/hsrm/mi/swtpro/backend/model/UserRights.java
+++ src/main/java/de/hsrm/mi/swtpro/backend/model/UserRights.java
@@ -2,7 +2,7 @@ package de.hsrm.mi.swtpro.backend.model;
 
 public enum UserRights {
 
-    ROLE_USER("USER"), ROLE_ADMIN("ADMIN");
+    USER("USER"), ADMIN("ADMIN");
 
     private final String userRights;
     UserRights(String userRights) {
