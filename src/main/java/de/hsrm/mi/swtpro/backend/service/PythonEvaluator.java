71a8ef7d137b26e3f216b4988e5ed04eaf8e70fe
Oliver Daniel Folwarzny <oliverdaniel.folwarzny@student.hs-rm.de>
2020-01-28 23:24:13 +0100

    Add swap function to server api, add function to evaluate python script

--- src/main/java/de/hsrm/mi/swtpro/backend/service/PythonEvaluator.java
+++ src/main/java/de/hsrm/mi/swtpro/backend/service/PythonEvaluator.java
@@ -1,13 +1,33 @@
 package de.hsrm.mi.swtpro.backend.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
+@Component
 public class PythonEvaluator {
 
     private ScriptEngine engine;
 
+    @Autowired
+    private ServerApi serverApi;
+
     public PythonEvaluator() {
         engine = new ScriptEngineManager().getEngineByName("python");
+    }
+
+    public void runScriptForSwapOffer(String script) {
+        try {
+            if(script.contains("def onNewSwapOffer():")) {
+                engine.put("serverApi",serverApi);
+                engine.eval(script);
+                engine.eval("onNewSwapOffer()");
+            }
+        } catch (ScriptException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/de/hsrm/mi/swtpro/backend/service/ServerApi.java
+++ src/main/java/de/hsrm/mi/swtpro/backend/service/ServerApi.java
@@ -1,21 +1,28 @@
 package de.hsrm.mi.swtpro.backend.service;
 
+import com.sun.tools.javac.util.ArrayUtils;
+import de.hsrm.mi.swtpro.backend.model.Group;
+import de.hsrm.mi.swtpro.backend.model.Student;
 import de.hsrm.mi.swtpro.backend.model.SwapOffer;
 import de.hsrm.mi.swtpro.backend.model.filter.Comparator;
 import de.hsrm.mi.swtpro.backend.model.filter.ComparatorType;
 import de.hsrm.mi.swtpro.backend.model.filter.Filter;
 import de.hsrm.mi.swtpro.backend.service.filterfactories.SwapOfferFilterFactory;
+import de.hsrm.mi.swtpro.backend.service.repository.StudentRepository;
 import de.hsrm.mi.swtpro.backend.service.repository.SwapOfferRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Component
 public class ServerApi {
 
     @Autowired
     SwapOfferRepository swapOfferRepository;
+    @Autowired
+    StudentRepository studentRepository;
 
     private SwapOffer latestSwapOffer;
 
@@ -58,11 +65,20 @@ public class ServerApi {
     }
 
     public boolean tripleSwap(long[] offerIds) {
-        //manually swap offers
-        // from D to A
-
-        // from A to B
-        // from B to D
+        assert offerIds.length == 3 : "size for offers is incorrect";
+        List<Long> ids = new ArrayList<>();
+        Arrays.stream(offerIds).forEach(ids::add);
+        List<SwapOffer> offers = this.swapOfferRepository.findAllById(ids);
+        Map<Long, SwapOffer> offerMap = offers.stream().collect(Collectors.toMap(swapOffer -> swapOffer.getStudent().getId(), swapOffer -> swapOffer));
+        List<Student> students = offers.stream().map((SwapOffer::getStudent)).collect(Collectors.toList());
+        students.forEach(student -> {
+            SwapOffer offer = offerMap.get(student.getId());
+            Set<Group> next = student.getGroups().stream().filter(group -> group.getId() == offer.getFromGroup().getId()).collect(Collectors.toSet());
+            next.add(offer.getToGroup());
+            student.setGroups(next);
+            studentRepository.save(student);
+        });
+        offers.forEach(swapOffer -> swapOfferRepository.delete(swapOffer));
         return true;
     }
 
