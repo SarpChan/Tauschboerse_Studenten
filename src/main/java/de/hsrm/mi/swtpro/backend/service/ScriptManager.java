f096e7f22179d6c285b67a53d5423f8c166f0f0d
Oliver Daniel Folwarzny <oliverdaniel.folwarzny@student.hs-rm.de>
2020-01-29 15:41:51 +0100

    Handle script loading and filtering via ScriptManager

--- src/main/java/de/hsrm/mi/swtpro/backend/service/ScriptManager.java
+++ src/main/java/de/hsrm/mi/swtpro/backend/service/ScriptManager.java
@@ -1,13 +1,32 @@
 package de.hsrm.mi.swtpro.backend.service;
 
+import de.hsrm.mi.swtpro.backend.model.Script;
 import de.hsrm.mi.swtpro.backend.service.repository.PythonScriptRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class ScriptManager {
 
     @Autowired
     PythonScriptRepository repository;
+
+    public List<Script> loadAllMatchingScriptsFor(String identifier) {
+        List<Script> scripts = repository.findAll();
+        filterScriptsFor(scripts, identifier);
+        Collections.shuffle(scripts);
+        return scripts;
+    }
+
+    private void filterScriptsFor(List<Script> scripts, String identifier) {
+        scripts = scripts.stream().filter(script ->
+                new String(script.getData()).contains(identifier)
+        ).collect(Collectors.toList());
+    }
 
 }
