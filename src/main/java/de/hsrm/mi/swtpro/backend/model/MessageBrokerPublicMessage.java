69242de17c46fe218343027c7fdb66784dd66e7e
= <nicklas.ahlers@stundent.hs-rm.de>
2020-01-29 12:49:52 +0100

    Models für MessageBroker erstellt

---
+++ src/main/java/de/hsrm/mi/swtpro/backend/model/MessageBrokerPublicMessage.java
@@ -0,0 +1,38 @@
+package de.hsrm.mi.swtpro.backend.model;
+
+import java.sql.Timestamp;
+
+public class MessageBrokerPublicMessage{
+    private long timestamp;
+    private String action;
+    private String data;
+
+
+    public MessageBrokerPublicMessage(String action, String data){
+        this.timestamp = new Timestamp(System.currentTimeMillis()).getTime();
+        this.action = action;
+        this.data = data;
+    }
+
+    public long getTimestamp(){
+        return this.timestamp;
+    }
+
+    public String getAction(){
+        return this.action;
+    }
+    
+    public String getData(){
+        return this.data;
+    }
+    /*
+         switch(action){
+            case "remove":
+                this.data = ((int)data) + "";
+                break;
+            case "add":
+                SwapOffer toSerialize = (SwapOffer) data;
+
+        }
+    */
+}
