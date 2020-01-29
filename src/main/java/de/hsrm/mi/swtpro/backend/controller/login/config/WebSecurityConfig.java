845dc34ac700273090c8d125a24f7a342948be7e
Leonie Hilgert <leonieluise.hilgert@student.hs-rm.de>
2020-01-29 13:24:48 +0100

    bug fix

--- src/main/java/de/hsrm/mi/swtpro/backend/controller/login/config/WebSecurityConfig.java
+++ src/main/java/de/hsrm/mi/swtpro/backend/controller/login/config/WebSecurityConfig.java
@@ -64,7 +64,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
           
                 .authorizeRequests().antMatchers("/authentication/**", "/h2-console/**").permitAll()
                 .antMatchers().hasAuthority("USER")		
-                .antMatchers("/rest/lists/**","rest/building/**","rest/campus/**", "rest/coursecomponents/**", "rest/curriculum/**", "rest/examregulation/**", "rest/fieldofstudy/**" ,
+                .antMatchers("rest/building/**","rest/campus/**", "rest/coursecomponents/**", "rest/curriculum/**", "rest/examregulation/**", "rest/fieldofstudy/**" ,
                 "rest/group/**", "rest/lecture/**", "rest/module/**", "rest/modulelnCurriculum/**", "rest/pyScribt/**", "rest/room/**", "rest/studentAttendsCourse/**", "rest/student/**", 
                 "rest/studentPassedExam/**", "rest/studentPrioritzesGroup/**", "rest/studyprogram/**", 
                 "rest/swapOffer/**", "rest/term/**", "rest/university/**", "rest/user/**").hasAuthority("ADMIN")			
