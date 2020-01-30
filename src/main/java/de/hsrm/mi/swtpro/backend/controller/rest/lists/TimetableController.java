c4c815c93943b5584154fa721c96cb26d9525b89
Lukas <dr.kaese@live.com>
2020-01-31 00:29:56 +0100

    TimetableUpdate laed jetzt auch ein TimetableModule hoch (durchs Frontend)

--- src/main/java/de/hsrm/mi/swtpro/backend/controller/rest/lists/TimetableController.java
+++ src/main/java/de/hsrm/mi/swtpro/backend/controller/rest/lists/TimetableController.java
@@ -1,51 +1,21 @@
 package de.hsrm.mi.swtpro.backend.controller.rest.lists;
 
-import de.hsrm.mi.swtpro.backend.controller.rest.CampusCrudController;
-import de.hsrm.mi.swtpro.backend.controller.rest.CourseComponentCrudController;
-import de.hsrm.mi.swtpro.backend.controller.rest.CourseCrudController;
-import de.hsrm.mi.swtpro.backend.controller.rest.GroupCrudController;
-import de.hsrm.mi.swtpro.backend.controller.rest.LecturerCrudController;
-import de.hsrm.mi.swtpro.backend.controller.rest.RoomCrudController;
-import de.hsrm.mi.swtpro.backend.model.Course;
-import de.hsrm.mi.swtpro.backend.model.CourseComponent;
-import de.hsrm.mi.swtpro.backend.model.ExamRegulation;
-import de.hsrm.mi.swtpro.backend.model.Group;
-import de.hsrm.mi.swtpro.backend.model.Lecturer;
 import de.hsrm.mi.swtpro.backend.model.Module;
-import de.hsrm.mi.swtpro.backend.model.Room;
-import de.hsrm.mi.swtpro.backend.model.ModuleInCurriculum;
-import de.hsrm.mi.swtpro.backend.model.TimetableModule;
-import de.hsrm.mi.swtpro.backend.model.User;
+import de.hsrm.mi.swtpro.backend.model.*;
 import de.hsrm.mi.swtpro.backend.model.filter.Comparator;
 import de.hsrm.mi.swtpro.backend.model.filter.ComparatorType;
 import de.hsrm.mi.swtpro.backend.model.filter.Filter;
 import de.hsrm.mi.swtpro.backend.service.filterfactories.ModuleFilterFactory;
-import de.hsrm.mi.swtpro.backend.service.repository.CourseComponentRepository;
-import de.hsrm.mi.swtpro.backend.service.repository.CourseRepository;
-import de.hsrm.mi.swtpro.backend.service.repository.GroupRepository;
-import de.hsrm.mi.swtpro.backend.service.repository.LecturerRepository;
 import de.hsrm.mi.swtpro.backend.service.helper.ServiceGenerator;
-import de.hsrm.mi.swtpro.backend.service.repository.ModuleInCurriculumRepository;
-import de.hsrm.mi.swtpro.backend.service.repository.ModuleRepository;
-import de.hsrm.mi.swtpro.backend.service.repository.RoomRepository;
-import de.hsrm.mi.swtpro.backend.service.repository.UserRepository;
-
+import de.hsrm.mi.swtpro.backend.service.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.time.DayOfWeek;
 import java.util.List;
 import java.util.Optional;
-
-import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/rest/lists")
@@ -118,24 +88,29 @@ public class TimetableController {
     /**
      * The methode handles the POST request
      * to update the modules of a timetable
-     * @param timetableModuleList
+     * @param
      * @return
      */
     @PostMapping(path = "/timetableUpdate", consumes = "application/json", produces = MediaType.APPLICATION_JSON_VALUE)
     public  ResponseEntity<String> updateTimetable(@RequestBody TimetableModule timetableModule) {
         try{
-            Group group = groupRepository.getOne(timetableModule.getGroupID());
+            Optional<Group> optionalGroup = groupRepository.findById(timetableModule.getGroupID());
+            if(!optionalGroup.isPresent())
+                return new ResponseEntity<>("Cant find GroupID in Database",HttpStatus.CONFLICT);
+
+            Group group = optionalGroup.get();
             CourseComponent courseComponent = group.getCourseComponent();
             Course course = group.getCourseComponent().getCourse();
             Room room = group.getRoom();
-            //group.setStartTime(timetableModule.getStartTime());
-            //group.setEndTime(timetableModule.getEndTime());
+            group.setStartTime(timetableModule.getStartTime());
+            group.setEndTime(timetableModule.getEndTime());
             group.setDayOfWeek(timetableModule.getDayOfWeek());
             group.setGroupChar(timetableModule.getGroupChar());
             course.setTitle(timetableModule.getCourseTitle());
-            courseComponent.setType(timetableModule.getCourseType());
+            //courseComponent.setType(timetableModule.getCourseType());
             room.setNumber(timetableModule.getRoomNumber());
-            groupRepository.save(group);
+
+            groupRepository.saveAndFlush(group);
         return new ResponseEntity<>("timetableUpdate Succses",HttpStatus.OK);
         }catch(Exception e){
             return new ResponseEntity<>(e.getMessage(),HttpStatus.CONFLICT);
