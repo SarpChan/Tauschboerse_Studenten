85751edbbca7e644b40c8560a56c7dd478d4abb6
Oliver Daniel Folwarzny <oliverdaniel.folwarzny@student.hs-rm.de>
2020-02-01 15:13:18 +0100

    Fix Constructor annotation in AuthenticationResponse

--- src/main/java/de/hsrm/mi/swtpro/backend/model/AuthenticationResponse.java
+++ src/main/java/de/hsrm/mi/swtpro/backend/model/AuthenticationResponse.java
@@ -1,12 +1,9 @@
 package de.hsrm.mi.swtpro.backend.model;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Builder
-@NoArgsConstructor
+@AllArgsConstructor
 public class AuthenticationResponse {
     @Getter
     @Setter
